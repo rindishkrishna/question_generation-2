30e3930abf9263c576135c4ee9fc9af7d335515c
rindish krishna <rindishkrishna@gmail.com>
2022-01-07 16:11:31 +0530

    Ui updated

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,9 @@
 let input = document.querySelector('#file');
 let dropzone = document.querySelector('.drop');
 let nameBox = document.querySelector('.drop span');
+let bt=document.getElementById("upload")
+bt.style.visibility="hidden";
+let file=''
 
 input.addEventListener('dragover', function () {
     dropzone.classList.add('active');
@@ -10,11 +13,18 @@ input.addEventListener('dragleave', function () {
     dropzone.classList.remove('active');
 });
 
+
 input.addEventListener('change', function () {
+    if (!input.files) {
+        return null;
+      }
+      file = input.files[0];
+    bt.style.visibility="visible";
     dropzone.classList.remove('active');
 
     //Take the file name and show it
-    let fileName = input.value;
+    fileName = input.value;
+
     let index = fileName.lastIndexOf('\\');
     fileName = fileName.substring(fileName.length, index + 1);
     nameBox.innerHTML = fileName;
@@ -29,24 +39,31 @@ let loadArrows = document.querySelector('.arrows');
 let checkmark = document.querySelector('.checkmark');
 
 uploadBtn.addEventListener('click', function () {
+
     if (nameBox.innerHTML !== '' && uploadBtn.innerHTML !== '') {
         this.innerHTML = 'Uploading...'
         loadBar.classList.add('active');
+        Tesseract.recognize(
+            file,
+            'eng',
+            { logger: m => console.log(m) }
+          ).then(({ data: { text } }) => {
+            document.getElementById("paragraph").value = text;
+            //Hide dropzone and show loading arrows
+            dropzone.classList.add('hidden');
+            loadArrows.classList.remove('hidden');
+            loadArrows.classList.add('active');
 
-        //Hide dropzone and show loading arrows
-        dropzone.classList.add('hidden');
-        loadArrows.classList.remove('hidden');
-        loadArrows.classList.add('active');
+            //After 1 second show checkmark
+            setTimeout(function () {
+                loadArrows.classList.add('hidden');
+                checkmark.classList.remove('hidden');
+                uploadBtn.innerHTML = '';
+                uploadBtn.remove()
 
-        //After 3 seconds show checkmark
-        setTimeout(function () {
-            loadArrows.classList.add('hidden');
-            checkmark.classList.remove('hidden');
-            uploadBtn.innerHTML = '';
-            uploadBtn.remove()
+            }, 1000);
+          })
 
-        }, 3000);
     }
 });
-
 
